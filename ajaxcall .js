342129b8f4925676cfed3f63acb8f394e012084d
varsha vellanki <70028775+cherryvarsha99@users.noreply.github.com>
2020-09-28 13:06:21 -0500

    Delete ajaxcall .js

--- ajaxcall .js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#ajaxcall").click(function(){   
-        $.get("https://restcountries.eu/rest/v2/name/eesti", function(data, status){ console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-        });
-    });
-});
